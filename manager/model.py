1b960bb1e54ea3cced51287b54f024351e4b4f60
Piero Valencia <piero_v@outlook.es>
2021-05-30 16:34:08 -0500

    Corrections

--- manager/model.py
+++ manager/model.py
@@ -4,18 +4,18 @@ import json
 class CSVFile:
     def __init__(self, path="data/alldata.csv", num_rows=1000):
         self._path = path
-        self._data_stores = None
 
     def create_file(self, data):
-        with open("data/alldata.csv", "w", newline="") as file:
+        with open(self._path, "w", newline="") as file:
             writer = csv.writer(file, delimiter=",")
             writer.writerows(data)
 
 class JSONFile:
     def __init__(self, path="data/data_config.json"):
         self._path = path
+        self._data_stores = None
 
     def load_file(self):
-        with open(self.path, "r", encoding="utf-8") as file:
+        with open(self._path, "r", encoding="utf-8") as file:
             self._data_stores = json.load(file)
             print("Data Loaded!", self._data_stores, sep="\n")
